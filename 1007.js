e11077b4ef8e7e235cb046be90e1488923f82b0d
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 20:39:25 -0300

    Create 1007.js

---
+++ 1007.js
@@ -0,0 +1,15 @@
+// ! Problem 1001 //
+
+var a = prompt('Digite aqui o valor do produto "a"')
+var b = prompt('Digite aqui o valor do produto "b"')
+var c = prompt('Digite aqui o valor do produto "c"')
+var d = prompt('Digite aqui o valor do produto "d"')
+
+var res1 = (Number.parseInt(a) * (Number.parseInt(b)))
+var res2 = (Number.parseInt(c) * (Number.parseInt(d)))
+
+var diferenca = res1 - res2
+
+console.log(diferenca)
+
+// It can't work in the website test, but it will work in your console (UGI bug) //
