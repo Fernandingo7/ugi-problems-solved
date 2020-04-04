ea43313a99e26baa352e644de042ae811c2059b7
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 20:32:03 -0300

    Create 1003.js

---
+++ 1003.js
@@ -0,0 +1,9 @@
+// ! Problem 1001 //
+
+let n1 = Number(lines.shift());
+let n2 = Number(lines.shift())
+let soma = n1 + n2;
+
+console.log(`Soma = ${soma}`)
+
+// It cant not work in the website, but it works in your console (UGI bug) //
