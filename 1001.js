c03775df9016b2c97661737dab5699097794d2f6
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 19:37:14 -0300

    Create 1001.js

---
+++ 1001.js
@@ -0,0 +1,8 @@
+// ! Problem 1001 // 
+
+var a = prompt('Defina o valor de a')
+var b = prompt('Defina o valor de b')
+var x = parseInt(a) + parseInt(b) // Muda para number! 
+console.log(x)
+
+// It cant not work in the website, but it works in your console (UGI bug) // 
