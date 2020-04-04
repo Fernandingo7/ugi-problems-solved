b936a525c9ca6479c85330553044085aba5138e7
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 20:36:38 -0300

    Update 1002.js

--- 1002.js
+++ 1002.js
@@ -4,4 +4,4 @@ let n = 3.14159
 let r = parseFloat(prompt('Qual o valor de r?'))
 console.log('A='+(n * Math.pow(r,2)).toFixed(4));
 
-// It cant not work in the website, but it works in your console (UGI bug) //
+// It can't work in the website test, but it will work in your console (UGI bug) //
