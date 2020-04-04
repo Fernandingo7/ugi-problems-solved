0ddbf68c35f5ba2614ced44b07eefe47305c5b3f
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 20:36:12 -0300

    Create 1005.js

---
+++ 1005.js
@@ -0,0 +1,19 @@
+// ! Problem 1001 //
+
+var a = prompt('Qual foi a primeira nota do aluno?')
+var b = prompt('Qual foi a segunda nota do aluno?')
+if (a > 10) {
+  console.log('Essa nota não é valida!')
+} else {
+}
+if (b > 10) {
+  console.log('Essa nota não é valida!')
+} else { 
+}
+
+
+var media = (Number.parseInt(a) + Number.parseInt(b)) / 2
+
+console.log(media)
+
+// It can't work in the website test, but it will work in your console (UGI bug) //
