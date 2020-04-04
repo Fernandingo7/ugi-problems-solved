c8d67268ed5105f73b3093a4b03f44c80e40a935
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 20:28:52 -0300

    Create ex002.js

---
+++ ex002.js
@@ -0,0 +1,14 @@
+  // ! Problem 1001 //
+  
+let n = 3.14159
+let r = parseFloat(prompt('Qual o valor de r?'))
+console.log('A='+(n * Math.pow(r,2)).toFixed(4));
+  
+  
+  
+  
+  
+  
+  
+  
+// It cant not work in the website, but it works in your console (UGI bug) //
