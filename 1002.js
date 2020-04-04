3470ac19f616cc5976c80c9259364cf8232b324b
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 20:29:27 -0300

    Update and rename ex002.js to 1002.js

--- 1002.js
+++ 1002.js
@@ -3,12 +3,5 @@
 let n = 3.14159
 let r = parseFloat(prompt('Qual o valor de r?'))
 console.log('A='+(n * Math.pow(r,2)).toFixed(4));
-  
-  
-  
-  
-  
-  
-  
-  
+
 // It cant not work in the website, but it works in your console (UGI bug) //
