f21d8456c16fc3530fc4525ac5f78701b1377b00
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 20:34:00 -0300

    Create 1004.js

---
+++ 1004.js
@@ -0,0 +1,9 @@
+// ! Problem 1001 //
+
+var num1 = Number(lines.shift())
+var num2 = Number(lines.shift())
+
+var PROD = num1*num2
+consule.log(`PROD =${PROD}`)
+
+// It cant not work in the website, but it works in your console (UGI bug) //
