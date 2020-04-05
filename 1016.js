5e082cef2cd55a4e85c70ac030ad5a52b94af2bc
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 21:35:44 -0300

    Create 1016.js

---
+++ 1016.js
@@ -0,0 +1,10 @@
+// ! Problem 1016 //
+
+const time = prompt('Digite aqui a distancia do carro X')
+
+const time1 = Number.parseInt(time)
+const result = time1 * 2 
+
+console.log(`${result} minutes`)
+
+// It can't work in the website test, but it will work in your console (UGI bug) //
