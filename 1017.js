195ce46858abd0b4d6c03da8911e23a5c4cee135
Fernandingo <61608814+Fernandingo7@users.noreply.github.com>
2020-04-04 21:36:29 -0300

    Create 1017.js

---
+++ 1017.js
@@ -0,0 +1,10 @@
+// ! Problem 1017 //
+
+const time = prompt('Digite aqui o tempo da viajem')
+const average = prompt('Digite aqui a velocidade média')
+
+const res = time * average / 12
+
+console.log(res.toFixed(3))
+
+// It can't work in the website test, but it will work in your console (UGI bug) //
